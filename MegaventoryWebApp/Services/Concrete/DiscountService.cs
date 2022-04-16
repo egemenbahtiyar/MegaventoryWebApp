556eec23f3a1d43f284c21aaff9fa67599d697a0
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-04-17 01:17:09 +0300

    Discount service was updated.

--- MegaventoryWebApp/Services/Concrete/DiscountService.cs
+++ MegaventoryWebApp/Services/Concrete/DiscountService.cs
@@ -2,7 +2,10 @@
 using MegaventoryWebApp.Services.Abstract;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MegaventoryWebApp.Services.Concrete
@@ -11,7 +14,28 @@ namespace MegaventoryWebApp.Services.Concrete
     {
         public void InsertDiscount(Discount discount)
         {
-            throw new NotImplementedException();
+            var url = "https://api.megaventory.com/v2017a/Discount/DiscountUpdate";
+            var responseDiscount = new Discount();
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+
+            httpRequest.Accept = "application/json";
+            httpRequest.ContentType = "application/json";
+
+            var data = JsonSerializer.Serialize(discount);
+
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write(data);
+            }
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                responseDiscount = JsonSerializer.Deserialize<Discount>(result);
+            }
+            var debug = httpResponse.StatusCode;
         }
     }
 }
