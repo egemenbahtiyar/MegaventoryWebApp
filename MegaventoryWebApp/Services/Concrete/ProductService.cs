dba2d069ea1cba2c442a99a107575714670befd8
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-04-17 01:23:09 +0300

    Product service was updated.

--- MegaventoryWebApp/Services/Concrete/ProductService.cs
+++ MegaventoryWebApp/Services/Concrete/ProductService.cs
@@ -2,7 +2,10 @@
 using MegaventoryWebApp.Services.Abstract;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MegaventoryWebApp.Services.Concrete
@@ -11,7 +14,29 @@ namespace MegaventoryWebApp.Services.Concrete
     {
         public void InsertProduct(Product product)
         {
-            throw new NotImplementedException();
+            var url = "https://api.megaventory.com/v2017a/Product/ProductUpdate";
+            var responseProduct = new Product();
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+
+            httpRequest.Accept = "application/json";
+            httpRequest.ContentType = "application/json";
+
+            var data = JsonSerializer.Serialize(product);
+
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write(data);
+            }
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                responseProduct = JsonSerializer.Deserialize<Product>(result);
+            }
+            var debug = httpResponse.StatusCode;
+
         }
     }
 }
