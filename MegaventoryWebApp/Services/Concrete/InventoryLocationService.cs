e39d010a0680f7608da389165d5e959df69545f4
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-04-17 00:34:04 +0300

    Abstract services were involved.

---
+++ MegaventoryWebApp/Services/Concrete/InventoryLocationService.cs
@@ -0,0 +1,17 @@
+﻿using MegaventoryWebApp.Models;
+using MegaventoryWebApp.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaventoryWebApp.Services.Concrete
+{
+    public class InventoryLocationService : IInventoryLocation
+    {
+        public void InsertInventoryLocation(InventoryLocation inventoryLocation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MegaventoryWebApp/Services/Concrete/ProductService.cs
@@ -0,0 +1,17 @@
+﻿using MegaventoryWebApp.Models;
+using MegaventoryWebApp.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaventoryWebApp.Services.Concrete
+{
+    public class ProductService : IProductService
+    {
+        public void InsertProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
