080dbe87e40ce49ccfa49b1a67cd7857949070f6
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-04-17 01:25:38 +0300

    Tax service was updated.

--- MegaventoryWebApp/Services/Concrete/TaxService.cs
+++ MegaventoryWebApp/Services/Concrete/TaxService.cs
@@ -2,7 +2,10 @@
 using MegaventoryWebApp.Services.Abstract;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MegaventoryWebApp.Services.Concrete
@@ -11,7 +14,28 @@ namespace MegaventoryWebApp.Services.Concrete
     {
         public void InsertTax(Tax tax)
         {
-            throw new NotImplementedException();
+            var url = "https://api.megaventory.com/v2017a/Tax/TaxUpdate";
+            var responseTax = new Tax();
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+
+            httpRequest.Accept = "application/json";
+            httpRequest.ContentType = "application/json";
+
+            var data = JsonSerializer.Serialize(tax);
+
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write(data);
+            }
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                responseTax = JsonSerializer.Deserialize<Tax>(result);
+            }
+            var debug = httpResponse.StatusCode;
         }
     }
 }
