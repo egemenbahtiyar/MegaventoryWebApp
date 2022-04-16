b99dc138c25381355fbb227b7e96edddd970bcef
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-04-16 16:20:19 +0300

    Models were created.

---
+++ MegaventoryWebApp/Models/InventoryLocation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaventoryWebApp.Models
+{
+
+    public class InventoryLocation
+    {
+        public string APIKEY { get; set; }
+        public Mvinventorylocation mvInventoryLocation { get; set; }
+        public string mvRecordAction { get; set; }
+        public string mvInsertUpdateDeleteSourceApplication { get; set; }
+    }
+
+    public class Mvinventorylocation
+    {
+        public string InventoryLocationName { get; set; }
+        public string InventoryLocationAbbreviation { get; set; }
+        public string InventoryLocationAddress { get; set; }
+   
+    }
+
+   
+}
---
+++ MegaventoryWebApp/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaventoryWebApp.Models
+{
+
+    public class Product
+    {
+        public string APIKEY { get; set; }
+        public Mvproduct mvProduct { get; set; }
+        public string mvRecordAction { get; set; }
+        public string mvInsertUpdateDeleteSourceApplication { get; set; }
+    }
+
+    public class Mvproduct
+    {
+        public string ProductSKU { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductSellingPrice { get; set; }
+        public double ProductPurchasePrice { get; set; }
+    }
+
+
+
+}
